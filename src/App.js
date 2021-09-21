678d63359f9ef94df2b8cf69d38905295e70a042
sami khalil <samihellstrong@gmail.com>
2021-09-21 15:09:17 +0200

    icons added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Button from '@mui/material/Button'
+import SaveIcon from '@mui/icons-material/Save';
 const App = () => {
   return (
     <div className='container'>
-<Button variant='contained'  >hello</Button>
+<Button  variant='contained'  > <SaveIcon/> Save</Button>
     </div>
   )
 }
