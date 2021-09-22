0f52687d9415eff0b5c58a0f0942245ca8f2143c
sami khalil <samihellstrong@gmail.com>
2021-09-23 00:02:46 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,86 +1,42 @@
-import {Container,Grid,Paper} from '@mui/material';
-import ButtonGroup from '@mui/material/ButtonGroup'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import  {useState}  from 'react'
-import SaveIcon from '@mui/icons-material/Save'
-import DeleteIcon from '@mui/icons-material/Delete'
-import CheckboxIcon from '@mui/material/Checkbox'
-import {makeStyles} from '@mui/styles'
-import Typography from '@mui/material/Typography';
-
 import 'fontsource-roboto'
-const useStyles=makeStyles({
-  root:{
-    background:'linear-gradient(45deg,#fe6888,#ff8e53)'
-    ,border:0,
-    borderRadius:15,
-    marginBottom:200,
-    padding:'5px,30px',
-    color:'white',
-    padding:0
-  }
-})
-let StyledButton=()=>{
-  let classes=useStyles()
-  return(<> <Button className={classes.root}>testing</Button>
-  <Typography variant='h1'>
-    welcome to my apap
-  </Typography>
-
-  </>
-    )}
-
-let CheckBox =()=>{
-  const [checked,setChecked]=useState(false)
-
-
-  return(
-    <>
-    <StyledButton/>
-  <TextField
-  label='name'
-   variant='outlined'
-   color='secondary' />
-    <CheckboxIcon 
-    checkedIcon={<DeleteIcon/>}
-    icon={<SaveIcon/>}
-    checked={checked} onChange={(e)=>{
-      setChecked(e.target.checked)
-    }} />
-  </>
-  )
-}
+import {Typography,Button,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,Toolbar,Container} from '@material-ui/core'
+import {PhotoCamera} from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
 const App = () => {
-  return ( <Container maxWidth='sm' style={{backgroundColor:'lightgray'}}> <div className='container'>
-    <CheckBox/>
-    <Grid container spacing={4} justifyContent='center'>
-    <Grid item >
-      <Paper style={{
-        height:75,width:50,
-      }} />
-      </Grid>
-      <Grid item >
-      <Paper style={{
-        height:75,width:50,
-      }} />
-      </Grid><Grid item >
-      <Paper style={{
-        height:75,width:50,
-      }} />
-      </Grid><Grid item >
-      <Paper style={{
-        height:75,width:50,
-      }} />
-      </Grid>
-    </Grid>
-    
-      <ButtonGroup>
-      <Button color='secondary' startIcon={<DeleteIcon/>}  variant='contained'  >  delete</Button>
+  return (
+    <>
+<CssBaseline/>
 
-<Button startIcon={<SaveIcon/>} color='primary' variant='contained'  >  Save</Button>
-  </ButtonGroup>  </div>
-  </Container>)  
+<AppBar color='secondary' position='relative' >
+  <Toolbar  >
+    <PhotoCamera/>
+    <Typography variant="h6" > photo album </Typography>
+  </Toolbar>
+</AppBar>
+<main>
+  <div>
+    <Container maxWidth='sm' >
+      <Typography variant='h2' align='center' color='textPrimary' gutterBottom  >
+Photo album 
+      </Typography>
+      <Typography variant='h5' align='center' color='textSecondary' paragraph  > 
+hello everbody i am trying to male this sentance as long as possible so everyone can read thsi shit
+</Typography>
+<Typography>
+  <Grid container spacing={2} justify='center' >
+    <Grid item >
+<Button variant='contained'color='primary' > see my photos </Button>
+    </Grid>
+    <Grid item >
+<Button variant='outlined'color='primary' > secondary action </Button>
+    </Grid>
+  </Grid>
+</Typography>
+    </Container>
+  </div>
+</main>
+</>
+  )
 }
 
 export default App
