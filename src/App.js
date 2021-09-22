5cd3bca6e0058b2158d9e57e7d201e8ab3c35ef7
sami khalil <samihellstrong@gmail.com>
2021-09-22 22:14:10 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Container,Grid,Paper} from '@mui/material';
 import ButtonGroup from '@mui/material/ButtonGroup'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
@@ -5,7 +6,10 @@ import  {useState}  from 'react'
 import SaveIcon from '@mui/icons-material/Save'
 import DeleteIcon from '@mui/icons-material/Delete'
 import CheckboxIcon from '@mui/material/Checkbox'
-import {makeStyles,ThemeProvider} from '@mui/styles'
+import {makeStyles} from '@mui/styles'
+import Typography from '@mui/material/Typography';
+
+import 'fontsource-roboto'
 const useStyles=makeStyles({
   root:{
     background:'linear-gradient(45deg,#fe6888,#ff8e53)'
@@ -19,8 +23,13 @@ const useStyles=makeStyles({
 })
 let StyledButton=()=>{
   let classes=useStyles()
-  return <Button className={classes.root}>testing</Button>
-}
+  return(<> <Button className={classes.root}>testing</Button>
+  <Typography variant='h1'>
+    welcome to my apap
+  </Typography>
+
+  </>
+    )}
 
 let CheckBox =()=>{
   const [checked,setChecked]=useState(false)
@@ -32,7 +41,7 @@ let CheckBox =()=>{
   <TextField
   label='name'
    variant='outlined'
-   color='primary' />
+   color='secondary' />
     <CheckboxIcon 
     checkedIcon={<DeleteIcon/>}
     icon={<SaveIcon/>}
@@ -43,16 +52,35 @@ let CheckBox =()=>{
   )
 }
 const App = () => {
-  return (
-<ThemeProvider>  
-    <div className='container'>
+  return ( <Container maxWidth='sm' style={{backgroundColor:'lightgray'}}> <div className='container'>
     <CheckBox/>
+    <Grid container spacing={4} justifyContent='center'>
+    <Grid item >
+      <Paper style={{
+        height:75,width:50,
+      }} />
+      </Grid>
+      <Grid item >
+      <Paper style={{
+        height:75,width:50,
+      }} />
+      </Grid><Grid item >
+      <Paper style={{
+        height:75,width:50,
+      }} />
+      </Grid><Grid item >
+      <Paper style={{
+        height:75,width:50,
+      }} />
+      </Grid>
+    </Grid>
+    
       <ButtonGroup>
-      <Button color='warning' startIcon={<DeleteIcon/>}  variant='contained'  >  delete</Button>
+      <Button color='secondary' startIcon={<DeleteIcon/>}  variant='contained'  >  delete</Button>
 
-<Button startIcon={<SaveIcon/>}  variant='contained'  >  Save</Button>
+<Button startIcon={<SaveIcon/>} color='primary' variant='contained'  >  Save</Button>
   </ButtonGroup>  </div>
-</ThemeProvider>)  
+  </Container>)  
 }
 
 export default App
