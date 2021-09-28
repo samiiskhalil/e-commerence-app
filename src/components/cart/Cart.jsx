bb24acf4b61d5bb16d266193c55aa3899592a860
sami khalil <samihellstrong@gmail.com>
2021-09-28 19:15:06 +0200

    commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -29,7 +29,7 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
         <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
         <div>
           <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
-          <Button component={Link} to='/checkout ' onClick={()=>window.location.reload()} className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+          <Button component={Link} to='/checkout'className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
         </div>
       </div>
     </>
